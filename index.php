18b26e4618a6fc0358f7de55cadd06c55243ebf6
Imy Pullan <imypullan@gmail.com>
2021-03-29 13:41:27 +0100

    function and head added

--- index.php
+++ index.php
@@ -10,19 +10,30 @@ $result = $query->fetchAll();
 ?>
 
 <html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="normalize.css">
+    <title>Booker Prize Winners</title>
+</head>
 <body>
 
 <h1>Booker Prize Winners</h1>
 
 <?php
-
-foreach ($result as $winner){
-    echo '<h2>Prize year: ' . $winner['prize_year'] . '</h2>';
-    echo '<span>Author: ' . $winner['author_name'] . '</span><br />';
-    echo '<span>Title: ' . $winner['book_name'] . '</span><br />';
-    echo '<span>Author nationality: ' . $winner['author_nationality'] . '</span><br />';
+function display_winners($result)
+{
+    foreach ($result as $winner) {
+        echo '<h2>Prize year: ' . $winner['prize_year'] . '</h2>';
+        echo '<span>Author: ' . $winner['author_name'] . '</span><br />';
+        echo '<span>Title: ' . $winner['book_name'] . '</span><br />';
+        echo '<span>Author nationality: ' . $winner['author_nationality'] . '</span><br />';
+    }
 }
+display_winners($result);
 ?>
+
+
+
 
 </body>
 </html>
