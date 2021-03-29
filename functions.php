b56fe77004527894b9d9b7413e3e41f69d3a26da
Imy Pullan <imypullan@gmail.com>
2021-03-29 14:45:37 +0100

    made form for submitting to DB

--- functions.php
+++ functions.php
@@ -1,6 +1,11 @@
 <?
-
-function display_winners(array $winners) :string
+/* displays list of DB info
+ *
+ * @param $winners array
+ *
+ * echoes list by field, no return
+ */
+function display_winners(array $winners)
 {
 foreach ($winners as $winner) {
 echo '<h2>Prize year: ' . $winner['prize_year'] . '</h2>';
--- index.php
+++ index.php
@@ -21,6 +21,19 @@ require_once 'functions.php';
 
 <h1>Booker Prize Winners</h1>
 
+<form action="addWinner.php">
+    <h4>Add other winners:</h4>
+    <label for="prize_year">Prize year</label><br />
+    <input type="number" placeholder="Year" /><br />
+    <label for="author_name">Author's name</label><br />
+    <input type="text" placeholder="Author's name" /><br />
+    <label for="book_title">Title</label><br />
+    <input type="text" placeholder="Title" /><br />
+    <label for="author_nationality">Author's nationality</label><br />
+    <input type="text" placeholder="Author's nationality" /><br />
+    <input type="submit" />
+</form>
+
 <?php
 display_winners($winners);
 ?>
