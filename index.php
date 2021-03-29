b788551e0f334e654e88910840f4b9352388d4d7
Imy Pullan <imypullan@gmail.com>
2021-03-29 11:44:59 +0100

    pointless whitespace

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $db = new PDO('mysql:host=db; dbname=booker_winners', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 $query = $db->prepare('SELECT `ID`, `prize_year`, `author_name`, `book_name`, `author_nationality` FROM `booker_winners`;');
-$query-> execute();
+$query->execute();
 $result = $query->fetchAll();
 
 echo '<pre>';
