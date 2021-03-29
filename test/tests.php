99b56910b31d3285f6e6c672fc724f04ecd6d67d
Imy Pullan <imypullan@gmail.com>
2021-03-29 14:55:20 +0100

    not working tests

--- test/tests.php
+++ test/tests.php
@@ -2,9 +2,10 @@
 
 use PHPUnit\Framework\TestCase;
 
-require_once '../functions.php';
+require '../functions.php';
 
-class Functions extends TestCase {
+class Functions extends TestCase
+{
     public function testDisplay_winners_success() {
         $winner = ['prize_year' => 2020, 'author_name' => 'Douglas Stuart', 'author_nationality' => 'Scottish-American'];
         display_winners($winner);
