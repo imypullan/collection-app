321c5c0a16505b56b4c1b95e3f3eaa91ef7395a2
Imy Pullan <imypullan@gmail.com>
2021-03-29 11:41:51 +0100

    task 3 peer review

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php
+
+$db = new PDO('mysql:host=db; dbname=booker_winners', 'root', 'password');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$query = $db->prepare('SELECT `ID`, `prize_year`, `author_name`, `book_name`, `author_nationality` FROM `booker_winners`;');
+$query-> execute();
+$result = $query->fetchAll();
+
+echo '<pre>';
+var_dump($result);
+echo '</pre>';
