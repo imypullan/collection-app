070eda0ce33c9a36d69409f4cb35dfae838bcca7
Imy Pullan <imypullan@gmail.com>
2021-03-31 09:35:37 +0100

    beginning styling

--- addWinner.php
+++ addWinner.php
@@ -7,6 +7,7 @@ $db = getDb();
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="style.css">
     <title>Booker Prize Winners</title>
 </head>
 
